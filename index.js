16037cdf57f95297b7a5ca16d30b0bc6d86c1ce3
hemanth.hm <hemanth.hm@gmail.com>
2015-06-21 01:29:29 +0530

    Better checks

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
+var isObject = require('is-object');
+var isArguments = require('is-arguments');
+var toStr = Object.prototype.toString;
 module.exports = function(nodes) {
-  var construct = {}.toString.call(nodes);
-  return typeof nodes === 'object' &&
-    /^\[object (HTMLCollection|NodeList|Object)\]$/.test(construct) &&
+  var construct = toStr.call(nodes);
+  return !isArguments(object) && isObject(nodes) &&
+    /^\[object (HTMLCollection|NodeList)\]$/.test(construct) &&
     nodes.length !== undefined &&
     (nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType >
       0));
